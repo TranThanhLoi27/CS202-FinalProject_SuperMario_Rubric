75acc42e57b04584512882f17913d84a4669db6c
Thanh Loi <thanhtranloi1912@gmail.com>
2026-06-14 12:51:47 +0700

    add Camera

--- include/Core/Camera.h
+++ include/Core/Camera.h
@@ -4,9 +4,10 @@
 
 class Camera{
 public:
-    Camera(sf:: View view, sf::Vector2f worldsize);
+    //set view, worldsize
+    Camera(sf::Vector2f viewsize, sf::Vector2f levelsize);
+    //view follows 2 players.
     void Change(const sf::Vector2f& p1, const sf::Vector2f& p2);
-    void SetWorldSize(const sf::Vector2f size);
 private:
     sf::View view;
     sf::Vector2f worldsize; 
--- src/Core/Camera.cpp
+++ src/Core/Camera.cpp
@@ -1,9 +1,13 @@
 #include <Core/Camera.h>
-
-Camera::Camera(sf::View view, sf::Vector2f worldsize){
-
-}
+#include <Utils/MathUtils.h>
+Camera::Camera(sf::Vector2f viewsize, sf::Vector2f levelsize): 
+    view(sf::Vector2f(viewsize.x / 2, viewsize.y / 2), viewsize),
+    worldsize(levelsize){}
 
 void Camera::Change(const sf::Vector2f& p1, const sf::Vector2f& p2){
-
+    sf::Vector2f center({(p1.x + p2.x) / 2, (p1.y + p2.y) / 2});
+    const sf::Vector2f viewsize = view.getSize();
+    center.x = MathUtils::clamp(center.x, viewsize.x / 2.f, worldsize.x - viewsize.x / 2.f);
+    center.y = MathUtils::clamp(center.y, viewsize.y / 2.f, worldsize.y - viewsize.y / 2.f);
+    view.setCenter(center);
 }
